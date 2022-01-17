93d89a7681baec9c4cc95ed22bb289e1df705eaf
Aditya Mitra <adityaarghya0@gmail.com>
2022-01-17 09:34:51 +0530

    Update storageoperations.py

--- storageoperations.py
+++ storageoperations.py
@@ -1,4 +1,6 @@
 from azure.storage.blob import *
+from datetime import *
+
 connect_str='DefaultEndpointsProtocol=https;AccountName=medrecordfiles;AccountKey=EbVY4ZM3XNywbJbeR6yDFCjJILD5Nqla3EPI7CU+B1QAXjk1wwstYTVPsxA1wyPuSBTGiO8d+uDBhg4Wa26yxw==;EndpointSuffix=core.windows.net'
 blob_service_client = BlobServiceClient.from_connection_string(connect_str)
 
@@ -7,7 +9,7 @@ usercontainer=blob_service_client.get_container_client('userfiles')
 
 def uploadUserFileToBlob(data, fln):
   blob_client = blob_service_client.get_blob_client(container='userfiles', blob=fln)
-  blob_client.upload_data(data)
+  blob_client.upload_blob(data)
   
 def getDownloadLink(fln):
   blob_sas_token= generate_blob_sas(
@@ -23,7 +25,7 @@ def getDownloadLink(fln):
 
 def uploadCryptoFile(data,fln):
   blob_client = blob_service_client.get_blob_client(container='cryptofiles', blob=fln)
-  blob_client.upload_data(data, overwrite=True)
+  blob_client.upload_blob(data, overwrite=True)
   
 def downloadCryptoFile(fln):
   blob_client = blob_service_client.get_blob_client(container='cryptofiles', blob=fln)
