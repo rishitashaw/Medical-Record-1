511ef8caac8b58d52d497ce936f766815f741d09
Aditya Mitra <adityaarghya0@gmail.com>
2022-01-17 09:27:14 +0530

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -20,7 +20,7 @@ import uuid
 import re
 
 url="medical-record.centralindia.cloudapp.azure.com"
-filepth='/home/vm_user/medrecords/'
+#filepth='/home/vm_user/medrecords/'
 regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
 
 createAllTables()
