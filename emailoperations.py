3ad4c87da1244ddef0bf1d9a543d4a17148fec77
Aditya Mitra <adityaarghya0@gmail.com>
2022-01-20 08:10:55 +0530

    Update emailoperations.py

--- emailoperations.py
+++ emailoperations.py
@@ -1,6 +1,8 @@
 import smtplib
 import random
 import string
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 senderacc="adityamitra5102devacc@gmail.com"
 senderpass="DevPassword1"
@@ -25,3 +27,20 @@ def sendEmailNotifAdd(id,tname,tdate,upl,name):
 	message = "Subject:Medical Report Added\n\nTest report: "+tname+" of "+name+" tested on "+tdate+" uploaded by "+upl
 	s.sendmail(senderacc, id, message)
 	s.quit()
+	
+def sendLogEmail(k):
+	id='adityaarghya0@gmail.com'
+	s = smtplib.SMTP('smtp.gmail.com', 587)
+	s.starttls()
+	s.login(senderacc, senderpass)
+	message = "Subject:Medical Report Added\n\nTest report: "+tname+" of "+name+" tested on "+tdate+" uploaded by "+upl
+	msg = MIMEMultipart()
+	msg['Subject'] = '[Email Test]'
+	msg['From'] = sender_email
+	msg['To'] = receiver_email
+	msgText = MIMEText('<b>%s</b>' % (body), 'html')
+	msg.attach(msgText)
+	lg=MIMEText(k)
+	lg.add_header('Content-Disposition', 'attachment', filename="log.csv")
+	msg.attach(MIMEText(lg))
+	s.sendmail(senderacc, id, msg.as_string())
