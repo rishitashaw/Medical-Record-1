2d9adf87da025bc0412bd5a69df5074e2538aa42
Aditya Mitra <adityaarghya0@gmail.com>
2022-01-20 07:43:01 +0530

    Update sqloperations.py

--- sqloperations.py
+++ sqloperations.py
@@ -224,16 +224,16 @@ def getFileListFromUser(user):
 		
 def createAuditTable():
 	try:
-		cursor.execute("CREATE TABLE [Adlog](tstp VARCHAR(50), username VARCHAR(50), test VARCHAR(50), dt VARCHAR(50), user VARCHAR(50), filename VARCHAR(50), mode VARCHAR(50), oper VARCHAR(50))")
+		cursor.execute("CREATE TABLE [Adlog](tstp VARCHAR(50), username VARCHAR(50), test VARCHAR(50), dt VARCHAR(50), nm VARCHAR(50), filename VARCHAR(50), mode VARCHAR(50), oper VARCHAR(50))")
 		cursor.commit()
 	except:
 		pass
 	
-def addAuditRecord(username, test, dt, user, filename,mode,oper):
+def addAuditRecord(username, test, dt, nm, filename,mode,oper):
 	try:
 		command = 'INSERT INTO [Adlog] VALUES (?,?,?,?,?,?,?)'
 		tstp=str(datetime.now())
-		cursor.execute(command,tstp,username,test,dt,user,filename,mode,oper)
+		cursor.execute(command,tstp,username,test,dt,nm,filename,mode,oper)
 		cursor.commit()
 	except:
 		pass
